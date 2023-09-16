aaf02609f8c55b5f7b3c35a4e181f615bf4c6cfe
Mohd-Ghouse123 <103202021+Mohd-Ghouse123@users.noreply.github.com>
2023-09-17 00:30:04 +0530

    commit

--- frontend/src/components/Startgame.js
+++ frontend/src/components/Startgame.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Redirect } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import io from "socket.io-client";
 import "./Startgame.css";
