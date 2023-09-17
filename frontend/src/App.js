1424023ae334f7b23b412ce439fdc004fb54d7be
Mohd-Ghouse123 <103202021+Mohd-Ghouse123@users.noreply.github.com>
2023-09-17 11:18:19 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-// src/App.js
-
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import Lobby from './components/Lobby';
 import Gameplay from './components/Gameplay';
 import StartGame from './components/Startgame';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
-
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Redirect from="/" to="/home" />
-        <Route path="/home" component={Home} />
-        <Route path="/lobby/:userId" component={Lobby} />
-        <Route path="/gameplay/:roomId/:userId" component={Gameplay} />
-        <Route path="/startgame/:roomId/:userId" component={StartGame} />
-       
-        {/* Add more routes for other pages */}
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/lobby/:userId" component={Lobby} />
+          <Route path="/gameplay/:roomId/:userId" component={Gameplay} />
+          <Route path="/startgame/:roomId/:userId" component={StartGame} />
+          {/* Add more routes for other pages */}
+          <Redirect from="/" to="/home" />
+        </Switch>
       </div>
     </Router>
   );
